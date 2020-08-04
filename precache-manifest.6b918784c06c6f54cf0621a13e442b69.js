79e5b0f4fb443be8a7b2f66f5fa64bb672ea54bf
gabrielpaoli <gabrielpaoli@hotmail.com>
2020-08-04 00:46:55 -0300

    Updates

---
+++ precache-manifest.6b918784c06c6f54cf0621a13e442b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5966f5a85012778453fc44a80be92aa",
+    "url": "/RobotReact/index.html"
+  },
+  {
+    "revision": "64ddb0a79f2042d03a1a",
+    "url": "/RobotReact/static/css/main.b3f0001a.chunk.css"
+  },
+  {
+    "revision": "7bb32111c585f6796179",
+    "url": "/RobotReact/static/js/2.87f73d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotReact/static/js/2.87f73d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ddb0a79f2042d03a1a",
+    "url": "/RobotReact/static/js/main.8e647618.chunk.js"
+  },
+  {
+    "revision": "c3635fb90de2a8085f92",
+    "url": "/RobotReact/static/js/runtime-main.1fe1851d.js"
+  }
+]);
